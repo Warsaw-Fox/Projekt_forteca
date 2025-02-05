d0904d9ebf7df47a54b05488eb369a55731ae68c
Warsaw-Fox <147930312+Warsaw-Fox@users.noreply.github.com>
2025-02-05 13:56:48 +0100

    adding files

---
+++ punbb/cache/cache_ext_version_notifications.php
@@ -0,0 +1,13 @@
+<?php
+
+if (!defined('FORUM_EXT_VERSIONS_LOADED')) define('FORUM_EXT_VERSIONS_LOADED', 1);
+
+$forum_ext_repos = array (
+);
+
+ $forum_ext_last_versions = array (
+);
+
+$forum_ext_versions_update_cache = 1736861706;
+
+?>
